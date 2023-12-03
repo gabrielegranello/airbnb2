9d8560d59095b6ec2b53ff709bbfeb06648e5fdc
Gabry <gabriele.granello@gmail.com>
2023-12-03 21:02:42 +0100

    added calendar component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 import {
   MagnifyingGlassCircleIcon,
   UserCircleIcon,
@@ -8,6 +12,22 @@ import {
   Bars3Icon,
 } from "@heroicons/react/24/solid";
 function Header() {
+  const [searchInput, setsearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const handleSelect = (ranges: any) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const selectionRange = {
+    key: "selection",
+    startDate: startDate,
+    endDate: endDate,
+  };
+
+  console.log(selectionRange);
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -21,6 +41,8 @@ function Header() {
       </div>
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setsearchInput(e.target.value)}
           className="pl-5 outline-none flex-grow bg-transparent text-sm text-gray-600 placeholder-gray-400"
           type="text"
           placeholder="Start your search"
@@ -35,6 +57,16 @@ function Header() {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+        </div>
+      )}
     </header>
   );
 }
